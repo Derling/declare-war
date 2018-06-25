58c58c5a100c1413f7cc910226b875dee0462dc7
Derling <derling0704@gmail.com>
2018-06-25 07:45:37 -0400

    Moved game logic functions to utility folder

--- src/components/util/constants.js
+++
@@ -1,5 +0,0 @@
-const RANK_VALUES = {"ace": 0, "2": 1, "3": 2, "4": 3, "5": 4, "6": 5, "7": 6, "8": 7, "9": 8, "10": 9, "jack": 10, "queen": 11, "king": 12};
-const SUIT_VALUES = {"♠": 0, "♥": 1, "♦": 2, "♣": 3};
-const SUIT_NAMES = {"♠": "clubs", "♦": "diamonds", "♥": "hearts", "♣": "spades"}
-
-export {RANK_VALUES, SUIT_VALUES, SUIT_NAMES};
--- src/components/util/index.js
+++
@@ -1,2 +0,0 @@
-export {shuffleCards, buildCards} from './functions.js';
-export {RANK_VALUES, SUIT_VALUES, SUIT_NAMES } from './constants.js';
